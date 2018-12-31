375ec838b1af8447426b17e6f887bb756eb65f80
Prantar <prantarbhowmik@yahoo.com>
2018-12-31 14:42:11 -0500

    Testing for checking valid move

--- sudoku_solver.go
+++ sudoku_solver.go
@@ -11,9 +11,9 @@ import (
 )
 
 const (
-	blockNum = 3
-	rowNum = 9
-	columnNum = 9
+	blockNum    = 3
+	rowNum      = 9
+	columnNum   = 9
 	sudokuSpots = 81
 )
 
@@ -25,9 +25,9 @@ func printTable(w io.Writer, text string) {
 
 		for block := 0; block < blockNum; block++ {
 			if block < blockNum-1 {
-				fmt.Fprint(w, rowBlock[blockNum*(block):blockNum*(block+1)] + "|")
+				fmt.Fprint(w, rowBlock[blockNum*(block):blockNum*(block+1)]+"|")
 			} else {
-				fmt.Fprintln(w, rowBlock[blockNum*(block) : blockNum*(block+1)])
+				fmt.Fprintln(w, rowBlock[blockNum*(block):blockNum*(block+1)])
 			}
 		}
 
@@ -37,21 +37,26 @@ func printTable(w io.Writer, text string) {
 	}
 }
 
+// check if a board is valid length
+func validBoardLength(text string) bool {
+	return len(text) == sudokuSpots
+}
+
 // return if a character is a board valid character or not
-func validBoardUnit(text rune) bool{
+func validBoardUnit(text rune) bool {
 	validCharacters := map[rune]bool{'1': true, '2': true, '3': true, '4': true,
-		                       '5': true, '6': true, '7': true, '8': true,
-													 '9': true}
+		'5': true, '6': true, '7': true, '8': true,
+		'9': true}
 
 	return validCharacters[text]
 }
 
 // convertCoorToArrLocation takes in board as string, boards row and column and
 // returns the value at the location
-func getValueAtRowColumn(text string, row, column int) string {
-	var location = columnNum * (row - 1) + (column - 1)
+func getValueAtRowColumn(text string, row, column int) rune {
+	var location = columnNum*(row-1) + (column - 1)
 
-	return text[location:location+1]
+	return []rune(text[location : location+1])[0]
 }
 
 // findFirstEmpty finds the first location in the puzzle that is empty
@@ -71,13 +76,13 @@ func safeInColumn(board string, value rune, column int) bool {
 	found[value] = true
 
 	for row := 1; row <= rowNum; row++ {
-		var valueAtLocation = []rune(getValueAtRowColumn(board, row, column))
+		valueAtLocation := getValueAtRowColumn(board, row, column)
 
-		if validBoardUnit(valueAtLocation[0]){
-			if found[valueAtLocation[0]] {
+		if validBoardUnit(valueAtLocation) {
+			if found[valueAtLocation] {
 				return false
 			}
-			found[valueAtLocation[0]] = true
+			found[valueAtLocation] = true
 		}
 	}
 
@@ -90,13 +95,13 @@ func safeInRow(board string, value rune, row int) bool {
 	found[value] = true
 
 	for column := 1; column <= columnNum; column++ {
-		var valueAtLocation = []rune(getValueAtRowColumn(board, row, column))
+		valueAtLocation := getValueAtRowColumn(board, row, column)
 
-		if validBoardUnit(valueAtLocation[0]) {
-			if found[valueAtLocation[0]] {
+		if validBoardUnit(valueAtLocation) {
+			if found[valueAtLocation] {
 				return false
 			}
-			found[valueAtLocation[0]] = true
+			found[valueAtLocation] = true
 		}
 	}
 
@@ -112,15 +117,15 @@ func safeInBlock(board string, value rune, row, column int) bool {
 	row = (row - 1) - ((row - 1) % 3) + 1
 	column = (column - 1) - ((column - 1) % 3) + 1
 
-	for blockRow := 1; blockRow <= rowNum / 3; blockRow++ {
-		for blockColumn := 1; blockColumn <= columnNum / 3; blockColumn++ {
-			var valueAtLocation = []rune(getValueAtRowColumn(board, row, column))
+	for blockRow := row; blockRow < row+3; blockRow++ {
+		for blockColumn := column; blockColumn < column+3; blockColumn++ {
+			valueAtLocation := getValueAtRowColumn(board, blockRow, blockColumn)
 
-			if validBoardUnit(valueAtLocation[0]) {
-				if found[valueAtLocation[0]] {
+			if validBoardUnit(valueAtLocation) {
+				if found[valueAtLocation] {
 					return false
 				}
-				found[valueAtLocation[0]] = true
+				found[valueAtLocation] = true
 			}
 		}
 	}
@@ -131,17 +136,13 @@ func safeInBlock(board string, value rune, row, column int) bool {
 // valueSafe determines if the value is safe to be placed in that
 // location of the puzzle
 func valueSafe(text string, value rune, row, column int) (status bool) {
-	return safeInColumn(text, value, column) && safeInRow(text, value, row) && safeInBlock(text, value, row, column)
+	return safeInColumn(text, value, column) && safeInRow(text, value, row) &&
+		safeInBlock(text, value, row, column)
 }
 
 // solves the sudoku puzzle
 func solvePuzzle(text string) string {
 	return text
-}
-
-// check if a board is valid length
-func validBoardLength(text string) bool {
-	return len(text) == sudokuSpots
 }
 
 // userInput takes a text and checks if the text is valid
--- sudoku_test.go
+++ sudoku_test.go
@@ -2,11 +2,277 @@ package main
 
 import "testing"
 
-func TestUserInput(t *testing.T) {
+func TestUserInput1(t *testing.T) {
 	input := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
 	value := userInput(input)
 
 	if value != input {
 		t.Errorf("Value was incorrect, got: %s, want %s", value, input)
+	}
+}
+
+func TestValidBoardLength1(t *testing.T) {
+	input := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	output := false
+
+	if validBoardLength(input) == output {
+		t.Errorf("Board does not have appropriate number of cells")
+	}
+}
+
+func TestValidBoardLength2(t *testing.T) {
+	input := "123456789"
+	output := true
+
+	if validBoardLength(input) == output {
+		t.Errorf("Board does not have appropriate number of cells")
+	}
+}
+
+func TestValidBoardUnit1(t *testing.T) {
+	input := '1'
+	output := false
+
+	if validBoardUnit(input) == output {
+		t.Errorf("%q is a valid character", input)
+	}
+}
+
+func TestValidBoardUnit2(t *testing.T) {
+	input := '@'
+	output := true
+
+	if validBoardUnit(input) == output {
+		t.Errorf("%q is not a valid character", input)
+	}
+}
+
+func TestValidBoardUnit3(t *testing.T) {
+	input := 'a'
+	output := true
+
+	if validBoardUnit(input) == output {
+		t.Errorf("%q is a not valid character", input)
+	}
+}
+
+func TestGetValueAtRowColumn1(t *testing.T) {
+	board := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	row, column := 1, 1
+	output := '1'
+
+	if getValueAtRowColumn(board, row, column) != output {
+		t.Errorf("%q is at the location", output)
+	}
+}
+
+func TestGetValueAtRowColumn2(t *testing.T) {
+	board := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	row, column := 5, 5
+	output := '5'
+
+	if getValueAtRowColumn(board, row, column) != output {
+		t.Errorf("%q is at the location", output)
+	}
+}
+
+func TestGetValueAtRowColumn3(t *testing.T) {
+	board := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	row, column := 9, 9
+	output := '9'
+
+	if getValueAtRowColumn(board, row, column) != output {
+		t.Errorf("%q is at the location", output)
+	}
+}
+
+func TestFindFirstEmpty1(t *testing.T) {
+	board := "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	output := -1
+
+	if findFirstEmpty(board) != output {
+		t.Errorf("There are no empty cells in the board")
+	}
+}
+
+func TestFindFirstEmpty2(t *testing.T) {
+	board := "a11111111222222222333333333444444444555555555666666666777777777888888888999999999"
+	output := 0
+
+	if findFirstEmpty(board) != output {
+		t.Errorf("The empty cell is at %d", output)
+	}
+}
+
+func TestFindFirstEmpty3(t *testing.T) {
+	board := "11111111122222222233333333344444444455555555566666666677777777788888888899999999."
+	output := 80
+
+	if findFirstEmpty(board) != output {
+		t.Errorf("The empty cell is at %d", output)
+	}
+}
+
+func TestFindFirstEmpty4(t *testing.T) {
+	board := "111111111222222222333333333z44444444555555555666.66666777777777888888888999999999"
+	output := 27
+
+	if findFirstEmpty(board) != output {
+		t.Errorf("The empty cell is at %d", output)
+	}
+}
+
+func TestSafeInColumn1(t *testing.T) {
+	board := ".11111111.22222222.33333333.44444444.55555555.66666666.77777777.88888888.99999999"
+	value := '1'
+	column := 1
+	output := true
+
+	if safeInColumn(board, value, column) != output {
+		t.Errorf("Placing value:%q at column:%d in board:%s is a valid move", value, column, board)
+	}
+}
+
+func TestSafeInColumn2(t *testing.T) {
+	board := "1111.11112222.22223333.33334444.44445555.55556666.66667777.77778888.88889999.9999"
+	value := '1'
+	column := 5
+	output := true
+
+	if safeInColumn(board, value, column) != output {
+		t.Errorf("Placing value:%q at column:%d in board:%s is a valid move", value, column, board)
+	}
+}
+
+func TestSafeInColumn3(t *testing.T) {
+	board := "1111.11112222222223333.33334444.44445555755556666.66667777677778888.8888999949999"
+	value := '1'
+	column := 5
+	output := true
+
+	if safeInColumn(board, value, column) != output {
+		t.Errorf("Placing value:%q at column:%d in board:%s is a valid move", value, column, board)
+	}
+
+}
+
+func TestSafeInColumn4(t *testing.T) {
+	board := "1111.11112222222223333.33334444144445555755556666.66667777677778888.8888999949999"
+	value := '1'
+	column := 5
+	output := false
+
+	if safeInColumn(board, value, column) != output {
+		t.Errorf("Placing value:%q at column:%d in board:%s is not a valid move", value, column, board)
+	}
+}
+
+func TestSafeInRow1(t *testing.T) {
+	board := ".........222222222333333333444444444555555555666666666777777777888888888999999999"
+	value := '1'
+	row := 1
+	output := true
+
+	if safeInRow(board, value, row) != output {
+		t.Errorf("Placing value:%q at row:%d in board:%s is a valid move", value, row, board)
+	}
+}
+
+func TestSafeInRow2(t *testing.T) {
+	board := "111111111222222222333333333444444444.........666666666777777777888888888999999999"
+	value := '1'
+	row := 5
+	output := true
+
+	if safeInRow(board, value, row) != output {
+		t.Errorf("Placing value:%q at row:%d in board:%s is a valid move", value, row, board)
+	}
+}
+
+func TestSafeInRow3(t *testing.T) {
+	board := "111111111222222222333333333444444444.2..7.6.4666666666777777777888888888999999999"
+	value := '1'
+	row := 5
+	output := true
+
+	if safeInRow(board, value, row) != output {
+		t.Errorf("Placing value:%q at row:%d in board:%s is a valid move", value, row, board)
+	}
+}
+
+func TestSafeInRow4(t *testing.T) {
+	board := "111111111222222222333333333444444444.2.17.6.4666666666777777777888888888999999999"
+	value := '1'
+	row := 5
+	output := false
+
+	if safeInRow(board, value, row) != output {
+		t.Errorf("Placing value:%q at row:%d in board:%s is not a valid move", value, row, board)
+	}
+}
+
+func TestSafeInBlock1(t *testing.T) {
+	board := "...111111...222222...333333444444444555555555666666666777777777888888888999999999"
+	value := '1'
+	row, column := 2, 2
+	output := true
+
+	if safeInBlock(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is a valid move", value, row, column, board)
+	}
+}
+
+func TestSafeInBlock2(t *testing.T) {
+	board := "111111111222222222333333333444...444555...555666...666777777777888888888999999999"
+	value := '1'
+	row, column := 6, 4
+	output := true
+
+	if safeInBlock(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is a valid move", value, row, column, board)
+	}
+}
+
+func TestSafeInBlock3(t *testing.T) {
+	board := "111111111222222222333333333444.2.444555.7.5556666.4666777777777888888888999999999"
+	value := '1'
+	row, column := 6, 5
+	output := true
+
+	if safeInBlock(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is a valid move", value, row, column, board)
+	}
+}
+
+func TestSafeInBlock4(t *testing.T) {
+	board := "111111111222222222333333333444.2.44455517.5556666.4666777777777888888888999999999"
+	value := '1'
+	row, column := 5, 6
+	output := false
+
+	if safeInBlock(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is not a valid move", value, row, column, board)
+	}
+}
+
+func TestValueSafe1(t *testing.T) {
+	board := ".6.14.57.9.......15.1...8..18...2.....46379.....8...42..6...2.48.......3.12.8..9."
+	value := '2'
+	row, column := 9, 9
+	output := false
+
+	if valueSafe(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is not a valid move", value, row, column, board)
+	}
+}
+
+func TestValueSafe2(t *testing.T) {
+	board := ".6.14.57.9.......15.1...8..18...2.....46379.....8...42..6...2.48.......3.12.8..9."
+	value := '1'
+	row, column := 6, 5
+	output := true
+
+	if valueSafe(board, value, row, column) != output {
+		t.Errorf("Placing value:%q at row:%d, column:%d in board:%s is a valid move", value, row, column, board)
 	}
 }
